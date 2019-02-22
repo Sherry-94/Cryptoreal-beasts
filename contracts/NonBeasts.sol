6933226d5aa91cd98e1cfc7cf612fd02f5a7008f
sherry5001 <shaheryar.akram@hotmail.com>
2019-02-22 05:53:53 +0500

    title added

---
+++ contracts/NonBeasts.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+/**
+ * @title NonBeasts
+ * @dev a contract holding all the functionality and logic for non beasts, i.e, simple animals
+ */
+ 
+contract NonBeasts {
+
+}
