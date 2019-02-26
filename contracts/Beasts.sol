81f10572fc355b9cf83e4ff775a6e38af4b8a65b
sherry5001 <shaheryar.akram@hotmail.com>
2019-02-27 01:47:00 +0500

    power and strength for non rare

--- contracts/Beasts.sol
+++ contracts/Beasts.sol
@@ -23,10 +23,12 @@ contract Beasts {
 
     // Type of power and it's strength
     struct power { 
+        uint pwID;
         string power;
         string pwDescription;
         uint pwStrength;
     }
+    mapping (uint => power) private powers;
 
     // Beast definition
     struct beast {
@@ -45,26 +47,31 @@ contract Beasts {
 
     function createBeast(string memory _name, Rarity _beastType) public {
         uint randPower = _generateRandomPower(_name);
-        _resolvePower(randPower, _beastType);
+        _resolveBeast(randPower, _beastType);
        // _createBeast(_name, randPower);
     }
+    
+    function _resolveBeast(uint _randPower, Rarity _beastType) private {
+        if(_beastType == Rarity.notRare)
+            _notRarePowers(_randPower, _beastType);
 
-    function _resolvePower(uint _randPower, Rarity _beastType) private {
-        if(_beastType == notRare)
-            _notRarePowers(_randPower);
+        else if(_beastType == Rarity.rare)
+            _rarePowers(_randPower, _beastType);
 
-        else if(_beastType == rare)
-            _rarePowers(_randPower);
+        else if(_beastType == Rarity.superRare)
+            _superRarePowers(_randPower, _beastType);
 
-        else if(_beastType == superRare)
-            _superRarePowers(_randPower);
-
-        else if(_beastType == utlraSuperRare)
-            _utlraSuperRarePowers(_randPower);
+        else if(_beastType == Rarity.utlraSuperRare)
+            _utlraSuperRarePowers(_randPower, _beastType);
 
         else
-             _mythicalPowers(_randPower);
+             _mythicalPowers(_randPower, _beastType);
     } 
+
+    function _notRarePowers(uint _randPower, Rarity _beastType) private view returns (uint) {
+        uint pw = _randPower / 10**(powerDigits-2);
+        uint pw1 = (pw / 10) + (pw % 10);
+    }
 
     
 }
